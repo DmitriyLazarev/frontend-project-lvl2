723b6e4fb74a42f9fea12bf55572aad3bf8aee91
Dmitry Lazarev <dmitry.lazarev@team.wrike.com>
2022-03-07 15:00:15 +0100

    add test coverage

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../index.js';
 
 test('genDiff', () => {
-  expect(genDiff('../__fixtures__/flatfile1.json', '../__fixtures__/flatfile2.json')).toBe('{\n'
+  expect(genDiff('__fixtures__/flatfile1.json', '__fixtures__/flatfile2.json')).toBe('{\n'
     + '  - follow: false\n'
     + '    host: hexlet.io\n'
     + '  - proxy: 123.234.53.22\n'
